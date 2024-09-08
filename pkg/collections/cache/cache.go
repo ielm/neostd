c2365ae51fd9db5a35f02778ab1050db461da1a4
ielm <i.leonmaldonado@gmail.com>
2024-09-08 17:19:12 -0400

    separate policy impls

--- pkg/collections/cache/cache.go
+++ pkg/collections/cache/cache.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/ielm/neostd/pkg/collections"
-	"github.com/ielm/neostd/pkg/collections/list"
 	"github.com/ielm/neostd/pkg/collections/maps"
 )
 
@@ -35,6 +34,8 @@ type Cache[K any] struct {
 }
 
 // NewCache creates a new cache with the given capacity and order policy
+// The comparator is used to compare keys in the cache, it's used by the underlying map
+// to find the item in O(1) time
 func NewCache[K any](capacity int, policy OrderPolicy[K], comparator collections.Comparator[K]) *Cache[K] {
 	return &Cache[K]{
 		capacity:   capacity,
@@ -120,180 +121,6 @@ func (c *Cache[K]) Size() int {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
 	return c.items.Size()
-}
-
-// LRUPolicy implements the Least Recently Used order policy
-type LRUPolicy[K any] struct {
-	list *list.LinkedList[*Item[K]]
-}
-
-func NewLRUPolicy[K any]() *LRUPolicy[K] {
-	return &LRUPolicy[K]{list: list.NewLinkedList[*Item[K]]()}
-}
-
-func (p *LRUPolicy[K]) Add(item *Item[K]) {
-	p.list.AddFirst(item)
-}
-
-func (p *LRUPolicy[K]) Remove(item *Item[K]) {
-	p.list.Remove(item)
-}
-
-func (p *LRUPolicy[K]) Update(item *Item[K]) {
-	p.Remove(item)
-	p.Add(item)
-}
-
-func (p *LRUPolicy[K]) Evict() *Item[K] {
-	if p.list.IsEmpty() {
-		return nil
-	}
-	item, _ := p.list.RemoveLast()
-	return item
-}
-
-// LFUPolicy implements the Least Frequently Used order policy
-type LFUPolicy[K any] struct {
-	frequencies *list.LinkedList[*FrequencyNode[K]]
-	items       map[*Item[K]]*list.Node[*Item[K]]
-	freqMap     map[int]*FrequencyNode[K]
-	minFreq     int
-}
-
-type FrequencyNode[K any] struct {
-	frequency int
-	items     *list.LinkedList[*Item[K]]
-}
-
-func NewLFUPolicy[K any]() *LFUPolicy[K] {
-	return &LFUPolicy[K]{
-		frequencies: list.NewLinkedList[*FrequencyNode[K]](),
-		items:       make(map[*Item[K]]*list.Node[*Item[K]]),
-		freqMap:     make(map[int]*FrequencyNode[K]),
-		minFreq:     0,
-	}
-}
-
-func (p *LFUPolicy[K]) Add(item *Item[K]) {
-	freqNode := p.getOrCreateFrequencyNode(1)
-	itemNode := freqNode.items.AddLast(item)
-	p.items[item] = itemNode
-	p.minFreq = 1
-}
-
-func (p *LFUPolicy[K]) Remove(item *Item[K]) {
-	if itemNode, ok := p.items[item]; ok {
-		freqNode := p.freqMap[item.frequency]
-		freqNode.items.RemoveNode(itemNode)
-		delete(p.items, item)
-
-		if freqNode.items.IsEmpty() {
-			p.removeFrequencyNode(freqNode)
-		}
-	}
-}
-
-func (p *LFUPolicy[K]) Update(item *Item[K]) {
-	p.Remove(item)
-	item.frequency++
-	freqNode := p.getOrCreateFrequencyNode(item.frequency)
-	itemNode := freqNode.items.AddLast(item)
-	p.items[item] = itemNode
-
-	if item.frequency-1 == p.minFreq && p.freqMap[p.minFreq] == nil {
-		p.minFreq = item.frequency
-	}
-}
-
-func (p *LFUPolicy[K]) Evict() *Item[K] {
-	if p.minFreq > 0 && p.freqMap[p.minFreq] != nil {
-		freqNode := p.freqMap[p.minFreq]
-		itemNode := freqNode.items.First()
-		if itemNode != nil {
-			item := itemNode.Value()
-			p.Remove(item)
-			return item
-		}
-	}
-	return nil
-}
-
-func (p *LFUPolicy[K]) getOrCreateFrequencyNode(freq int) *FrequencyNode[K] {
-	if freqNode, ok := p.freqMap[freq]; ok {
-		return freqNode
-	}
-
-	freqNode := &FrequencyNode[K]{
-		frequency: freq,
-		items:     list.NewLinkedList[*Item[K]](),
-	}
-	p.freqMap[freq] = freqNode
-	// Insert the new frequency node in the correct position
-	var insertAfter *list.Node[*FrequencyNode[K]]
-	for node := p.frequencies.First(); node != nil; node = node.Next() {
-		if node.Value().frequency > freq {
-			break
-		}
-		insertAfter = node
-	}
-
-	if insertAfter == nil {
-		p.frequencies.AddFirst(freqNode)
-	} else {
-		p.frequencies.AddAfter(insertAfter, freqNode)
-	}
-
-	return freqNode
-}
-
-func (p *LFUPolicy[K]) removeFrequencyNode(freqNode *FrequencyNode[K]) {
-	p.frequencies.Remove(freqNode)
-	delete(p.freqMap, freqNode.frequency)
-}
-
-// LFRUPolicy implements the Least Frequently/Recently Used order policy
-type LFRUPolicy[K any] struct {
-	lfu *LFUPolicy[K]
-	lru *LRUPolicy[K]
-}
-
-func NewLFRUPolicy[K any]() *LFRUPolicy[K] {
-	return &LFRUPolicy[K]{
-		lfu: NewLFUPolicy[K](),
-		lru: NewLRUPolicy[K](),
-	}
-}
-
-func (p *LFRUPolicy[K]) Add(item *Item[K]) {
-	p.lfu.Add(item)
-	p.lru.Add(item)
-}
-
-func (p *LFRUPolicy[K]) Remove(item *Item[K]) {
-	p.lfu.Remove(item)
-	p.lru.Remove(item)
-}
-
-func (p *LFRUPolicy[K]) Update(item *Item[K]) {
-	p.lfu.Update(item)
-	p.lru.Update(item)
-}
-
-func (p *LFRUPolicy[K]) Evict() *Item[K] {
-	lfuItem := p.lfu.Evict()
-	lruItem := p.lru.Evict()
-
-	if lfuItem == lruItem {
-		return lfuItem
-	}
-
-	if lfuItem.frequency < lruItem.frequency {
-		p.lru.Add(lruItem)
-		return lfuItem
-	}
-
-	p.lfu.Add(lfuItem)
-	return lruItem
 }
 
 // Update the createNewPolicy method to include the new policies
---
+++ pkg/collections/cache/lru_policy.go
@@ -0,0 +1,32 @@
+package cache
+
+import "github.com/ielm/neostd/pkg/collections/list"
+
+// LRUPolicy implements the Least Recently Used order policy
+type LRUPolicy[K any] struct {
+	list *list.LinkedList[*Item[K]]
+}
+
+func NewLRUPolicy[K any]() *LRUPolicy[K] {
+	return &LRUPolicy[K]{list: list.NewLinkedList[*Item[K]]()}
+}
+
+func (p *LRUPolicy[K]) Add(item *Item[K]) {
+	p.list.AddFirst(item)
+}
+
+func (p *LRUPolicy[K]) Remove(item *Item[K]) {
+	p.list.Remove(item)
+}
+
+func (p *LRUPolicy[K]) Update(item *Item[K]) {
+	p.list.MoveToFront(item)
+}
+
+func (p *LRUPolicy[K]) Evict() *Item[K] {
+	if p.list.IsEmpty() {
+		return nil
+	}
+	item, _ := p.list.RemoveLast()
+	return item
+}
