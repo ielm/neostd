0e380fc9230a335173b4412b754aeb30502e5c54
ielm <i.leonmaldonado@gmail.com>
2024-09-08 13:05:17 -0400

    use collections.Comparator[T]

--- pkg/collections/list/linkedlist.go
+++ pkg/collections/list/linkedlist.go
@@ -2,6 +2,8 @@
 
 package list
 
+import "github.com/ielm/neostd/pkg/collections"
+
 type node[T any] struct {
 	value T
 	next  *node[T]
@@ -12,7 +14,7 @@ type LinkedList[T any] struct {
 	head       *node[T]
 	tail       *node[T]
 	size       int
-	comparator func(T, T) bool
+	comparator collections.Comparator[T]
 }
 
 func NewLinkedList[T any]() *LinkedList[T] {
@@ -34,7 +36,7 @@ func (l *LinkedList[T]) Add(item T) bool {
 	return true
 }
 
-func (l *LinkedList[T]) SetComparator(comp func(T, T) bool) {
+func (l *LinkedList[T]) SetComparator(comp collections.Comparator[T]) {
 	l.comparator = comp
 }
 
@@ -47,7 +49,7 @@ func (l *LinkedList[T]) Remove(item T) bool {
 		panic("Comparator not set for non-comparable type")
 	}
 
-	if l.comparator(l.head.value, item) {
+	if l.comparator(l.head.value, item) == 0 {
 		l.head = l.head.next
 		if l.head == nil {
 			l.tail = nil
@@ -60,7 +62,7 @@ func (l *LinkedList[T]) Remove(item T) bool {
 
 	current := l.head
 	for current.next != nil {
-		if l.comparator(current.next.value, item) {
+		if l.comparator(current.next.value, item) == 0 {
 			current.next = current.next.next
 			if current.next == nil {
 				l.tail = current
@@ -74,4 +76,39 @@ func (l *LinkedList[T]) Remove(item T) bool {
 	}
 
 	return false
+}
+
+// New method to insert in sorted order
+func (l *LinkedList[T]) InsertSorted(item T) {
+	if l.comparator == nil {
+		panic("Comparator not set for non-comparable type")
+	}
+
+	newNode := &node[T]{value: item}
+
+	if l.head == nil || l.comparator(item, l.head.value) <= 0 {
+		newNode.next = l.head
+		if l.head != nil {
+			l.head.prev = newNode
+		}
+		l.head = newNode
+		if l.tail == nil {
+			l.tail = newNode
+		}
+	} else {
+		current := l.head
+		for current.next != nil && l.comparator(item, current.next.value) > 0 {
+			current = current.next
+		}
+		newNode.next = current.next
+		newNode.prev = current
+		if current.next != nil {
+			current.next.prev = newNode
+		} else {
+			l.tail = newNode
+		}
+		current.next = newNode
+	}
+
+	l.size++
 }
